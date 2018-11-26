4ad0c2bec2226e033612082428397c2523c23a11
FilipeAugustoVO <43390392+FilipeAugustoVO@users.noreply.github.com>
2018-11-26 18:47:52 -0300

    Add files via upload

--- CadastroClientes/Properties/Settings.Designer.cs
+++ CadastroClientes/Properties/Settings.Designer.cs
@@ -8,22 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CadastroClientes.Properties
-{
-
-
+namespace CadastroClientes.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string TextoPessoasFisicas {
+            get {
+                return ((string)(this["TextoPessoasFisicas"]));
+            }
+            set {
+                this["TextoPessoasFisicas"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string TextoPessoasJuridicas {
+            get {
+                return ((string)(this["TextoPessoasJuridicas"]));
+            }
+            set {
+                this["TextoPessoasJuridicas"] = value;
             }
         }
     }
